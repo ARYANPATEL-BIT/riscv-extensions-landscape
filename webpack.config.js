a7ebb658cdfea31de007edd3c79bb398b0aeb815
ARYANPATEL-BIT <aryan.patel7291@gmail.com>
2026-04-26 23:37:52 +0530

    chore: add webpack-dev-server and dynamic development config
    Signed-off-by: ARYANPATEL-BIT <aryan.patel7291@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,49 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  mode: 'production',
-  entry: './src/main.jsx',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react']
+module.exports = (env, argv) => {
+  const isDev = argv.mode !== 'production';
+
+  return {
+    mode: isDev ? 'development' : 'production',
+    entry: './src/main.jsx',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: isDev ? 'eval-source-map' : undefined,
+    devServer: isDev ? {
+      port: 3000,
+      hot: true,
+      open: true,
+      historyApiFallback: true,
+    } : undefined,
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react']
+            }
           }
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader', 'postcss-loader']
         }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader']
-      }
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        filename: 'index.html'
+      })
     ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: 'index.html'
-    })
-  ]
+  };
 };
